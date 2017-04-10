8dfec53f3a94dbaa6798985b775bef3ae4f45a37
josarsepi <olacheajeremy@gmail.com>
2017-04-10 17:03:30 -0400

    Update findinline.py

--- findinline.py
+++ findinline.py
@@ -14,7 +14,7 @@ def managed(func):
 
 class findinline:
 	
-	# from csvfindinline import csvfindinline
+	# from findinline import findinline
 	# finder = findinline()
 	# finder.makenew()
 	
@@ -42,7 +42,7 @@ class findinline:
 
 		for each in self.findlist:
 			place = self.findlist.index(each)
-			neweach = '(' + str(each) + ')'
+			neweach = '.*(' + str(each) + ')'
 			del self.findlist[place]
 			self.findlist.insert(place,neweach)
 			
@@ -55,105 +55,95 @@ class findinline:
 		import os
 		import io
 		
-		print(str(self.comreg))
+		print("What I think the regex is:\n" + str(self.comreg))
+		print("What the regex actually is:\n" + str(self.regex.pattern))
 		
-		print("Searching these files: " + str(self.filelist) + "\n For these things: " + str(self.findlist))
+		#print("Searching these files: " + str(self.filelist) + "\n For these things: " + str(self.findlist))
 		
-		self.biglist = self.searchfiles(self.filelist,self.findlist)
+		self.searchfiles(self.filelist,self.findlist)
 
-		betterlog = open(self.output, 'w')
-		for littlelist in self.biglist:
-			for record in littlelist:
-				betterlog.write(str(record))
-			
-		betterlog.close
 		print("Total Lines: " + str(self.totline))
 		print("Total time: " + str(self.tottime))
 		
 	def searchfiles(self,filelst,findlst):
 		import os
 		
-		biglist = []
-		
-		print("Createing BIGLIST from LITTLELISTS.")
-		
 		print("Here's your isother statement: " + str(self.isother))
 		
 		if self.isother == None:
 			for each in filelst:
-				self.littlelist = self.finder(each,self.findlist)
-				for those in self.littlelist:
-					biglist.append([those])
+				self.finder(each,self.findlist)
+
 		elif str(self.isother) == 'csv':
 			for each in filelst:
-				self.littlelist = self.csvfinder(each,self.findlist)
-				for those in self.littlelist:
-					biglist.append([those])
-				
-		print("Finished creating Biglist.")
-		
-		return biglist
+				self.csvfinder(each,self.findlist)
 		
 	@managed
 	def finder(self, path2file,findlist):
 		import os
 		import io
+		from io import FileIO, BufferedWriter
 		import re
 		
-		print("Creating a LITTLELIST from " + str(path2file) + ".")
+		print("Adding matches from " + str(path2file) + " to output: " + str(self.output))
 		
 		file2read = path2file
-		littlelist = []
+		
 
-		with open(file2read) as f:
+		with open(file2read, 'r') as f:
 			rowcount = 0
 			matchcount = 0
 			
-			line = f.readline()
-			while line is not None:
-				rowcount = rowcount + 1
-				nugget = re.search(self.regex,line)
-				if nugget == None:
-					matchcount = matchcount + 1
-					putter = str(line)
-					littlelist.append(putter)
+			with BufferedWriter(FileIO(self.output,'wb'),buffer_size=409600) as outf:
+			
 				line = f.readline()
+				while line is not None:
+					rowcount = rowcount + 1
+					nugget = re.search(self.regex,line)
+					if nugget == None:
+							outf.write(str.encode('\n'.join(line)))
+							matchcount = matchcount + 1
+					else:
+						pass
+					line = f.readline()
+
+			outf.close
 			
 			self.totline = self.totline + rowcount
-			length = len(littlelist)
-			print("Done creating or updating a LITTLELIST with " + str(length) + " objects.")
-			print(rowcount)
-			print(matchcount)
-
-		return littlelist
+			print("Done adding objects to file " + str(self.output))
+			
+			print("Total Rows in " + str(path2file) + " : " + str(rowcount))
+			print("Total Matches in " + str(path2file) + " : " + str(matchcount))
 	
 	@managed
 	def csvfinder(self,path2file,findlist):
 		import os
 		import io
+		from io import FileIO, BufferedWriter
 		import re
 		import csv
 		
-		print("Creating a LITTLELIST from " + str(path2file) + ".")
-
-		littlelist = []
+		print("Adding matches from " + str(path2file) + " to output: " + str(self.output))
 		
 		with open(path2file, newline=self.newline, encoding='utf8') as csvfile:
-			f = csv.reader(csvfile)
+
 			rowcount = 0
 			matchcount = 0
-			for row in f:
-				rowcount = rowcount + 1
-				nugget = re.search(self.regex,str(row))
-				if nugget != None:
-					matchcount = matchcount + 1
-					putter = row
-					littlelist.append(putter)
+			with BufferedWriter(FileIO(self.output,'wb'),buffer_size=409600) as outf:
+				f = csv.reader(csvfile)
+				for row in f:
+					rowcount = rowcount + 1
+					full = str(row)
+					nugget = re.match(self.regex,full)
+					if nugget != None:
+						outf.write(str.encode(full + '\n'))
+						matchcount = matchcount + 1
+
+			outf.close
 			
 			self.totline = self.totline + rowcount
-			length = len(littlelist)
-			print("Done creating or updating a LITTLELIST with " + str(length) + " objects.")
-			print("Total Rows in " + str(path2file) + " : " + rowcount)
-			print("Total Matches in " + str(path2file) + " : " + matchcount)
+			print("Done adding objects to file " + str(self.output))
 			
-		return littlelist
+			print("Total Rows in " + str(path2file) + " : " + str(rowcount))
+			print("Total Matches in " + str(path2file) + " : " + str(matchcount))
+
