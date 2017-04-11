5ab49f1af8a4ea13c56714b5259dbb79c16cc4eb
josarsepi <olacheajeremy@gmail.com>
2017-04-11 16:44:30 -0400

    Fixed
    Fixed regex, fixed char errors, fixed memory errors related to large files, optimized regex creation.

--- findinline.py
+++ findinline.py
@@ -14,136 +14,88 @@ def managed(func):
 
 class findinline:
 	
-	# from findinline import findinline
-	# finder = findinline()
-	# finder.makenew()
-	
 	def __init__(self):
 		
 		import re
 		
 		from finderconf import filelist
 		from finderconf import findlist
-		from finderconf import isother
 		from finderconf import output
-		from finderconf import newline
 		
 		self.filelist = filelist
 		self.findlist = findlist
 		self.output = output
-		self.isother = isother
-		self.newline = newline
 		self.totline = 0
 		self.tottime = 0
-		self.littelist = None
-		self.biglist = None
-		self.newline = None
 		self.comreg = None
 
-		for each in self.findlist:
-			place = self.findlist.index(each)
-			neweach = '.*(' + str(each) + ')'
-			del self.findlist[place]
-			self.findlist.insert(place,neweach)
+#################################
+#
+#################################
 			
 		comreg = '|'.join(self.findlist)
-		self.comreg = 'r' + "'" + str(comreg) + "'"
+		self.comreg = r'.*(' + str(comreg) + ')'
 		self.regex = re.compile(str(self.comreg))
+		print("Searching for: " + str(self.regex.pattern))
+		
+##################################
+#
+##################################
 	
 	def makenew(self):
 		
 		import os
 		import io
+		from io import FileIO, BufferedWriter
 		
-		print("What I think the regex is:\n" + str(self.comreg))
-		print("What the regex actually is:\n" + str(self.regex.pattern))
-		
-		#print("Searching these files: " + str(self.filelist) + "\n For these things: " + str(self.findlist))
-		
-		self.searchfiles(self.filelist,self.findlist)
+		with BufferedWriter(FileIO(self.output,'wb'),buffer_size=4096000) as self.outf:
+			self.searchfiles(self.filelist,self.findlist)
+		self.outf.close
 
 		print("Total Lines: " + str(self.totline))
 		print("Total time: " + str(self.tottime))
-		
+
+#################################
+#
+#################################
+
 	def searchfiles(self,filelst,findlst):
 		import os
 		
-		print("Here's your isother statement: " + str(self.isother))
-		
-		if self.isother == None:
-			for each in filelst:
-				self.finder(each,self.findlist)
-
-		elif str(self.isother) == 'csv':
-			for each in filelst:
-				self.csvfinder(each,self.findlist)
+		for each in filelst:
+			self.finder(each,self.findlist)
+			
+#################################
+#
+#################################
 		
 	@managed
-	def finder(self, path2file,findlist):
+	def finder(self, file2read,findlist):
 		import os
 		import io
-		from io import FileIO, BufferedWriter
+		from io import FileIO, BufferedReader, BufferedWriter
 		import re
 		
-		print("Adding matches from " + str(path2file) + " to output: " + str(self.output))
+		print("Adding matches from " + str(file2read) + " to output: " + str(self.output))
 		
-		file2read = path2file
-		
+		#with BufferedWriter(FileIO(self.output,'wb'),buffer_size=409600) as outf:
 
-		with open(file2read, 'r') as f:
+		with open(file2read, 'r', encoding='utf-8') as f:
 			rowcount = 0
 			matchcount = 0
-			
-			with BufferedWriter(FileIO(self.output,'wb'),buffer_size=409600) as outf:
-			
+				
+			line = f.readline()
+			while line != '':
+				rowcount = rowcount + 1
+				nugget = re.match(self.regex,str(line))
+				if nugget != None:
+					self.outf.write(str.encode(line))
+					matchcount = matchcount + 1
+
 				line = f.readline()
-				while line is not None:
-					rowcount = rowcount + 1
-					nugget = re.search(self.regex,line)
-					if nugget == None:
-							outf.write(str.encode('\n'.join(line)))
-							matchcount = matchcount + 1
-					else:
-						pass
-					line = f.readline()
 
-			outf.close
-			
-			self.totline = self.totline + rowcount
-			print("Done adding objects to file " + str(self.output))
-			
-			print("Total Rows in " + str(path2file) + " : " + str(rowcount))
-			print("Total Matches in " + str(path2file) + " : " + str(matchcount))
-	
-	@managed
-	def csvfinder(self,path2file,findlist):
-		import os
-		import io
-		from io import FileIO, BufferedWriter
-		import re
-		import csv
+		self.totline = self.totline + rowcount
+		print("Done adding objects to file " + str(self.output))
 		
-		print("Adding matches from " + str(path2file) + " to output: " + str(self.output))
-		
-		with open(path2file, newline=self.newline, encoding='utf8') as csvfile:
-
-			rowcount = 0
-			matchcount = 0
-			with BufferedWriter(FileIO(self.output,'wb'),buffer_size=409600) as outf:
-				f = csv.reader(csvfile)
-				for row in f:
-					rowcount = rowcount + 1
-					full = str(row)
-					nugget = re.match(self.regex,full)
-					if nugget != None:
-						outf.write(str.encode(full + '\n'))
-						matchcount = matchcount + 1
-
-			outf.close
-			
-			self.totline = self.totline + rowcount
-			print("Done adding objects to file " + str(self.output))
-			
-			print("Total Rows in " + str(path2file) + " : " + str(rowcount))
-			print("Total Matches in " + str(path2file) + " : " + str(matchcount))
-
+		print("Total Lines in " + str(file2read) + " : " + str(rowcount))
+		print("Total Matches in " + str(file2read) + " : " + str(matchcount))
